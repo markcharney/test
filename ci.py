f2cbdf02dca798fd21a96dd18dc0f4123918dd68
Mark Charney <mark.charney@intel.com>
2017-06-13 10:07:44 -0400

    travis

---
+++ ci.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import os
+import sys
+import platform
+import subprocess
+
+#for size in ['ia32','x86-64']:
+#    for link in ['','--shared']:
+#        cmd = 'python mfile.py --build-dir=build host_cpu=%s %s test' % (size,link)
+#        print(cmd)
+#        subprocess.check_call(cmd, shell=True)
+
+print("hello")
+print(sys.executable)
+sys.exit(0)
