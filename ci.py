5061cd794df4fc850c9b9b6bfc87012f56e85f44
Mark Charney <mark.charney@intel.com>
2018-03-15 14:31:43 -0400

    trying latest python2/3 config for travis

--- ci.py
+++ ci.py
@@ -4,12 +4,12 @@ import sys
 import platform
 import subprocess
 
-#for size in ['ia32','x86-64']:
-#    for link in ['','--shared']:
-#        cmd = 'python mfile.py --build-dir=build host_cpu=%s %s test' % (size,link)
-#        print(cmd)
-#        subprocess.check_call(cmd, shell=True)
 
+for py in ['python', 'python3']:
+        cmd = '{} --version'.format(py)
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
+        
 print("hello")
 print(sys.executable)
 sys.exit(0)
